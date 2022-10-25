f5ae66dea3889fd96c1ba48635c7baed921cb77d
Manuel Angel Gómez Allende <81536814+mgallende@users.noreply.github.com>
2022-10-25 23:58:54 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        var txt = "el resultado de la suma es: "; // Texto pàra concantenar con el resultado numérico
+        var txt = "El resultado de la suma es: "; // Texto pàra concantenar con el resultado numérico
 
         int num1 = 3;
         int num2 = 6;
         int num3 = 7;
         var sum = suma (num1, num2, num3);
 
-        System.out.println((txt) + sum);
-
-        var txtpuertas = "El número de puertas de mi Coche es :";
+        System.out.println((txt) + sum); // muestra el resultado con el texto concatenado
+        
+       
+        var txtpuertas = "El número de puertas de mi Coche es :"; // Texto para concatenar con el resultado del número de puertas
         coche miCoche = new coche();
         miCoche.incrementarPuertas();
-        System.out.println((txtpuertas) + miCoche.puertas);
+        System.out.println((txtpuertas) + miCoche.puertas); //muestra el número de puertas con el texto concatenado
     }
 
 
