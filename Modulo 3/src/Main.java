e9312dbafcad0416ef02bd8fb301f6730538c2cd
Manuel Angel Gómez Allende <81536814+mgallende@users.noreply.github.com>
2022-10-26 00:38:18 +0200

    Add files via upload

---
+++ Modulo 3/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+        var txt = "el resultado de la suma es: "; // Texto pàra concantenar con el resultado numérico
+
+        int num1 = 3;
+        int num2 = 6;
+        int num3 = 7;
+        var sum = suma (num1, num2, num3);
+
+        System.out.println((txt) + sum);
+
+        var txtpuertas = "El número de puertas de mi Coche es :";
+        coche miCoche = new coche();
+        miCoche.incrementarPuertas();
+        System.out.println((txtpuertas) + miCoche.puertas);
+    }
+
+
+
+    public static int suma(int a, int b, int c) {
+       return a + b + c ;
+    }
+}
+
+
+class coche {
+    public int puertas = 4;
+
+    public void incrementarPuertas(){
+        this.puertas++;
+    }
+}
